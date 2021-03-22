71da3c4d716356aacf445b8b255c17056ffa59ad
gviegas91 <48496739+gviegas91@users.noreply.github.com>
2021-03-22 16:04:25 -0300

    Comment morgan import

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const app = express();
 const studentsRoute = require("./api/routes/students");
 
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(express.json());
 app.use("/students", studentsRoute);
 
