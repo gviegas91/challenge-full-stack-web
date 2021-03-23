0cdb924a81ba1f0667fdb3c468b9fdda64689d23
gviegas91 <48496739+gviegas91@users.noreply.github.com>
2021-03-23 09:50:12 -0300

    Test 2

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 const studentsRoute = require("./api/routes/students");
 
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(express.json());
 
 app.use((req, res, next) => {
