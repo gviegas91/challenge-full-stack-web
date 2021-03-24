acc29a3d8c243068aae41c2890a4f85d67e8c3d6
gviegas91 <48496739+gviegas91@users.noreply.github.com>
2021-03-24 18:52:17 -0300

    Deploy

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,15 @@
 const express = require("express");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 const studentsRoute = require("./api/routes/students");
 
-// app.use(morgan("dev"));
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
