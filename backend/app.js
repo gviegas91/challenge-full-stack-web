aaa9282d93ee19a3f1fc545f2fd5f37e8ec33bdb
gviegas91 <48496739+gviegas91@users.noreply.github.com>
2021-03-24 18:46:25 -0300

    Change CORS Header and errors handler.
    - Add PATCH method in cors header.
    - Create errors handler.
    - Modify POST, PATCH and DELETE.

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
