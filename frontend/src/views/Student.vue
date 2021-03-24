579fdff57b8606d93ead0a6400c2e7ad128472cb
gviegas91 <48496739+gviegas91@users.noreply.github.com>
2021-03-24 18:43:45 -0300

    API integrations, validations and error handler.
    - Installed Vue the mask.
    - Create alert to show success or erros api calls.
    - Create validation fields.
    - API integration.

--- frontend/src/views/Student.vue
+++ frontend/src/views/Student.vue
@@ -1,121 +1,158 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="studentList"
-    class="elevation-1"
-    item-key="ra"
-    :search="search"
-    hide-default-footer
-  >
-    <template v-slot:top>
-      <v-toolbar flat height="80">
-        <v-text-field
-          v-model="search"
-          placeholder="Digite sua busca"
-          class="mx-4"
-          append-icon="mdi-magnify"
-        ></v-text-field>
+  <div>
+    <v-progress-linear
+      indeterminate
+      color="primary"
+      v-show="loading"
+    ></v-progress-linear>
+    <v-data-table
+      :headers="headers"
+      :items="studentList"
+      class="elevation-1"
+      item-key="ra"
+      :search="search"
+      hide-default-footer
+      loading-text="Carregando itens"
+    >
+      <template v-slot:top>
+        <v-toolbar flat height="80">
+          <v-text-field
+            v-model="search"
+            placeholder="Digite sua busca"
+            class="mx-4"
+            append-icon="mdi-magnify"
+          ></v-text-field>
 
-        <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
 
-        <v-dialog v-model="dialog" max-width="600px">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-              Cadastrar aluno
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6">
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Nome"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6">
-                    <v-text-field
-                      v-model="editedItem.email"
-                      label="Email"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6">
-                    <v-text-field
-                      v-model="editedItem.ra"
-                      label="RA"
-                      :disabled="disableField"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6">
-                    <v-text-field
-                      v-model="editedItem.cpf"
-                      label="CPF"
-                      :disabled="disableField"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close">
-                Cancelar
+          <v-dialog v-model="dialog" max-width="600px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+                Cadastrar aluno
               </v-btn>
-              <v-btn color="blue darken-1" text @click="save">
-                Cadastrar
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="600px">
-          <v-card>
-            <v-card-title class="headline"
-              >Tem certeza que deseja deletar este aluno?</v-card-title
-            >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete"
-                >Cancelar</v-btn
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-form ref="form">
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6">
+                        <v-text-field
+                          v-model="editedItem.name"
+                          label="Nome"
+                          :rules="[formRules.required, formRules.nameRule]"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <v-text-field
+                          v-model="editedItem.email"
+                          label="Email"
+                          type="email"
+                          :rules="[formRules.required, formRules.emailRule]"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <v-text-field
+                          v-model="editedItem.ra"
+                          label="RA"
+                          type="tel"
+                          :disabled="disableField"
+                          :rules="disableField ? [] : [formRules.required]"
+                          v-mask="'#############'"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <v-text-field
+                          v-model="editedItem.cpf"
+                          label="CPF"
+                          :disabled="disableField"
+                          :rules="
+                            disableField
+                              ? []
+                              : [formRules.required, formRules.cpfRule]
+                          "
+                          v-mask="'###.###.###-##'"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-form>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">
+                  Cancelar
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="save">
+                  Cadastrar
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="600px">
+            <v-card>
+              <v-card-title class="headline"
+                >Tem certeza que deseja deletar o aluno
+                {{ editedItem.name }}?</v-card-title
               >
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                >Deletar</v-btn
-              >
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-    <template v-slot:[`item.actions`]="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-    </template>
-    <template v-slot:no-data>
-      <v-card class="loader" flat min-height="200">
-        <v-progress-circular
-          indeterminate
-          color="primary"
-        ></v-progress-circular>
-      </v-card>
-    </template>
-    <template v-slot:no-result> Nenhum resultado encontrado </template>
-  </v-data-table>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete"
+                  >Cancelar</v-btn
+                >
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                  >Deletar</v-btn
+                >
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-card flat min-height="200">
+          <v-card-subtitle>Nenhum aluno cadastrado</v-card-subtitle>
+        </v-card>
+      </template>
+      <template v-slot:no-result> Nenhum resultado encontrado </template>
+    </v-data-table>
+    <v-alert
+      dense
+      elevation="2"
+      :type="alert.type"
+      transition="scale-transition"
+      v-show="alert.status"
+    >
+      {{ alert.message }}
+      <v-icon small @click="closeAlert()"> mdi-close-circle </v-icon>
+    </v-alert>
+  </div>
 </template>
-//
+
 <script>
+import { mask } from 'vue-the-mask';
 import axios from 'axios';
 import endpoints from '../api/endpoints';
 
 export default {
   name: 'Student',
-
+  directives: { mask },
   data: () => ({
+    alert: {
+      status: false,
+      message: '',
+      type: 'success',
+    },
+    loading: false,
     disableField: false,
     search: '',
     studentList: [],
@@ -128,6 +165,15 @@ export default {
     dialog: false,
     dialogDelete: false,
     editedIndex: -1,
+    formRules: {
+      required: (val) => (val && val.length > 0) || 'Este campo é obrigatório.',
+      nameRule: (val) =>
+        (val && val.length > 3) || 'Nome precisa ter no mínimo 3 caracteres.',
+      emailRule: (val) =>
+        (val && /.+@.+\..+/.test(val)) || 'Email precisa ser válido.',
+      cpfRule: (val) =>
+        (val && val.length >= 14) || 'CPF precisa ter 11 caracteres.',
+    },
   }),
   computed: {
     headers() {
@@ -158,6 +204,21 @@ export default {
     },
   },
   methods: {
+    closeAlert() {
+      this.alert.status = false;
+    },
+
+    showAlert(message, type = 'success') {
+      this.alert.status = true;
+      this.alert.message = message;
+      this.alert.type = type;
+    },
+
+    resetForm() {
+      this.$refs.form.resetValidation();
+      this.$refs.form.reset();
+    },
+
     editItem(item) {
       this.disableField = true;
       this.editedIndex = this.studentList.indexOf(item);
@@ -172,13 +233,25 @@ export default {
     },
 
     deleteItemConfirm() {
-      this.studentList.splice(this.editedIndex, 1);
-      this.closeDelete();
+      this.loading = true;
+      axios
+        .delete(endpoints.students, { data: this.editedItem })
+        .then((response) => {
+          this.studentList.splice(this.editedIndex, 1);
+          this.loading = false;
+          this.closeDelete();
+          this.showAlert(response.data.message);
+        })
+        .catch((e) => {
+          this.loading = false;
+          this.showAlert(e.response.data.message, 'error');
+        });
     },
 
     close() {
       this.disableField = false;
       this.dialog = false;
+      this.resetForm();
       this.$nextTick(() => {
         this.editedItem = { ...this.defaultItem };
         this.editedIndex = -1;
@@ -194,34 +267,54 @@ export default {
     },
 
     save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.studentList[this.editedIndex], this.editedItem);
-      } else {
-        this.studentList.push(this.editedItem);
-        axios
-          .post(endpoints.students, this.editedItem)
-          .then((response) => {
-            console.log('ADDDDD ', response);
-            this.studentList.push(response.data.response); // mudar
-          })
-          .catch((e) => {
-            console.log('ERROR ', e);
-            this.errors.push(e);
-          });
+      if (this.$refs.form.validate()) {
+        this.loading = true;
+        if (this.editedIndex > -1) {
+          axios
+            .patch(endpoints.students, this.editedItem)
+            .then((response) => {
+              Object.assign(
+                this.studentList[this.editedIndex],
+                this.editedItem,
+              );
+              this.loading = false;
+              this.close();
+              this.showAlert(response.data.message);
+            })
+            .catch((e) => {
+              this.loading = false;
+              this.close();
+              this.showAlert(e.response.data.message, 'error');
+            });
+        } else {
+          axios
+            .post(endpoints.students, this.editedItem)
+            .then((response) => {
+              this.loading = false;
+              this.close();
+              this.showAlert(response.data.message);
+              this.studentList.push(response.data.result);
+            })
+            .catch((e) => {
+              this.loading = false;
+              this.close();
+              this.showAlert(e.response.data.message, 'error');
+            });
+        }
       }
-      this.close();
     },
   },
   mounted() {
+    this.loading = true;
     axios
       .get(endpoints.students)
       .then((response) => {
-        console.log('RESPONSE ', response);
-        this.studentList = response.data.response; // mudar
+        this.loading = false;
+        this.studentList = response.data;
       })
       .catch((e) => {
-        console.log('ERROR ', e);
-        this.errors.push(e);
+        this.loading = false;
+        this.showAlert(e.response.data.message, 'error');
       });
   },
 };
@@ -236,9 +329,19 @@ export default {
   align-items: center;
 }
 
-.loader {
+.v-data-table__empty-wrapper .v-card {
   align-items: center;
   display: flex;
   justify-content: center;
+}
+
+.v-alert {
+  bottom: 0;
+  position: fixed;
+  right: 10px;
+}
+
+.v-alert .v-icon {
+  margin: 0 0 0 40px;
 }
 </style>
